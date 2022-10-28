d240af408e3b8a2b73d06fd81cae188b67d9e2d5
OliveiraJPE <peoj2009@gmail.com>
2022-10-28 12:35:45 -0300

    retidada das pastas

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ var hora = data.getHours()
 msg.innerHTML=`Agora são ${hora} horas`
 if(hora>=0 && hora<12){
     msg.innerHTML+= `<br> Bom dia!`
-    img.src=`_imagens/manhaRed.png`
+    img.src=`manhaRed.png`
     window.document.body.style.background= `#e2cd9f`
 }else if(hora>=12 && hora<18){
     msg.innerHTML+= `<br> Boa tarde!`
-    img.src=`_imagens/tardeRed.png`
+    img.src=`tardeRed.png`
     window.document.body.style.background= `#b9846f`
 }else{
     msg.innerHTML+= `<br> Boa noite!`
-    img.src=`_imagens/noiteRed.png`
+    img.src=`noiteRed.png`
     window.document.body.style.background= `#515154`
 }
 }
